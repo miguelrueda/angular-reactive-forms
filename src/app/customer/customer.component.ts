d3cf791b1ac85330d6e08f3a35d2893b6ce1b29e
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-09-11 15:37:42 -0500

    Module 5 completed

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,6 +1,28 @@
 import { OnInit, Component } from '@angular/core';
 import { Customer } from './customer';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+
+function emailMatcher(c: AbstractControl): {[key: string]: boolean} | null {
+  const emailControl = c.get('email');
+  const confirmControl = c.get('confirmEmail');
+
+  if (emailControl.pristine || confirmControl.pristine) {
+    return null;
+  }
+  if (emailControl.value === confirmControl.value) {
+    return null;
+  }
+  return { match: true };
+}
+
+function ratingRange(min: number, max: number): ValidatorFn {
+
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value !== null && (isNaN(c.value) || c.value < min || c.value > max)) {
+      return { range: true };
+    }
+  };
+}
 
 @Component({
   selector: 'app-customer',
@@ -9,15 +31,57 @@ import { NgForm } from '@angular/forms';
 })
 export class CustomerComponent implements OnInit {
 
+  customerForm: FormGroup;
   customer = new Customer();
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.customerForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.maxLength(50)]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        confirmEmail: ['', Validators.required],
+      }, {
+        validator: emailMatcher
+      }),
+      sendCatalog: true,
+      phone: '',
+      rating: [null, ratingRange(1, 5)],
+      notification: 'email'
+    });
+    // this.customerForm = new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true)
+    // });
+  }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log(`Saved`, JSON.stringify(customerForm.value));
+  save() {
+    console.log(this.customerForm);
+    console.log(`Saved`, JSON.stringify(this.customerForm.value));
+  }
+
+  public populateTestData() {
+    // setValue requires all values for the form, patchValue requires only as requested
+    this.customerForm.patchValue({
+      firstName: 'Jack',
+      lastName: 'Harkness',
+      // email: 'jack@torchwood.com',
+      sendCatalog: true
+    });
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.customerForm.get('phone');
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required);
+    } else {
+      phoneControl.clearValidators();
+    }
+    phoneControl.updateValueAndValidity();
   }
 
 }
